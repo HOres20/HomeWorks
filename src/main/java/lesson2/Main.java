b29daf4c44f1c3eb465f00a5ba842db4be44b116
Tleu <tlbakizov@gmail.com>
2022-03-25 17:16:02 +0600

    Второе задание

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -3,10 +3,16 @@ package lesson2;
 public class Main {
     public static void main(String[] args) {
         int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int[] arr2 = new int[8];
         Reverse(arr1);
         printArr(arr1);
+        insertArr();
     }
 
+    /**
+     * Метод меняющий 0 в массиве на 1 и наоборот.
+     * @param arr массив
+     */
     private static void Reverse(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
@@ -18,7 +24,20 @@ public class Main {
         }
     }
 
+    private static void insertArr() {
 
+    }
+
+
+
+
+
+
+
+    /**
+     * Метод выводит массив на экран
+     * @param arr
+     */
     private static void printArr(int[] arr) {
         for (int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
