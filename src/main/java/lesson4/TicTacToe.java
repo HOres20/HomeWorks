a66cac08f8dbd7c62e9a2b1502d8431ab79c70e4
Tleu <HOres20@yandex.ru>
2022-03-31 00:18:59 +0600

    Create TicTacToe.java
    Первые шаги

---
+++ src/main/java/lesson4/TicTacToe.java
@@ -0,0 +1,9 @@
+package lesson4;
+
+public class TicTacToe {
+    private static final int SIZE = 3;
+
+    private static final char DOT_EMPTY = '•';
+    private static final char DOT_HUMAN = 'X';
+    private static final char DOT_AI = '0';
+}
