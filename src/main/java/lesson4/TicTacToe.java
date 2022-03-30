02afed48a4adba90f5bd74f9c596f8d98dcbdc5e
Tleu <HOres20@yandex.ru>
2022-03-31 00:21:48 +0600

    Update TicTacToe.java
    Объявляем массив

--- src/main/java/lesson4/TicTacToe.java
+++ src/main/java/lesson4/TicTacToe.java
@@ -1,9 +1,16 @@
 package lesson4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class TicTacToe {
     private static final int SIZE = 3;
 
     private static final char DOT_EMPTY = '•';
     private static final char DOT_HUMAN = 'X';
     private static final char DOT_AI = '0';
+
+    private static  char[][] MAP ;
+    private static final Scanner in = new Scanner(System.in);
+    private static final Random random = new Random();
 }
